8b861bd3abc6ec013b6e71415882e6e908d5212c
Surajy001 <soorajy001@gmail.com>
2023-01-06 05:05:36 +0530

    Counter page added

---
+++ pages/counter.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+    const [count , setCount]=useState(0);
+  return (
+    <div>
+        <h1>Counter App</h1>
+        <h2>Count : {count}</h2>
+        <button onClick={()=>setCount(count + 1)}>INC</button>
+        <button onClick={()=>setCount(count - 1)}>DEC</button>
+        </div>
+  )
+}
+
+export default Counter
