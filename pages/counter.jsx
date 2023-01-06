d5ca090a9b2b80a94756373fc19caf8c4f87a642
Surajy001 <soorajy001@gmail.com>
2023-01-06 05:38:39 +0530

    deploy gh and vercel again

--- pages/counter.jsx
+++ pages/counter.jsx
@@ -4,7 +4,7 @@ const Counter = () => {
     const [count , setCount]=useState(0);
   return (
     <div>
-        <h2>Count : {count}</h2>
+        <h2>Count: {count}</h2>
         <button onClick={()=>setCount(count + 1)}>INC</button>
         <button onClick={()=>setCount(count - 1)}>DEC</button>
         </div>
